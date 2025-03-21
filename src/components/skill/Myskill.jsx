1ab6892d42212c0c8b952bc65630a93f3ad2f248
dipak chavan <dc741094@gmail.com>
2025-03-21 12:34:23 +0530

    new again

--- src/components/skill/Myskill.jsx
+++ src/components/skill/Myskill.jsx
@@ -1,44 +1,6 @@
 import "./skill.css";
-import { useRef ,useEffect,useState} from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function MySkill() {
-  // GSAP animation setup
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Check if the device is mobile
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Adjust breakpoint as needed
-    };
-    handleResize(); // Check on mount
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  const containerRef = useRef(null);
-  gsap.registerPlugin(ScrollTrigger);
-
-  useGSAP(() => {
-  if(!isMobile){
-    gsap.from(".card", {
-      opacity: 0,
-      y: 100,
-      duration: 1,
-      stagger: 0.5, // Smooth stagger effect for each card
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "top 80%",
-        end: "top 30%",
-        scrub:1,
-      },
-    });
-  }
-  }, [isMobile]);
-
   // Skill data
   const skills = [
     { name: "HTML", img: "/HTML5.png" },
@@ -52,7 +14,7 @@ function MySkill() {
   ];
 
   return (
-    <section ref={containerRef} className="Skill-card">
+    <section className="Skill-card">
       {skills.map((skill, index) => (
         <div key={index} className="card">
           <div className="card-img">
